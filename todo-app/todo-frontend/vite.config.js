7a108c01a213f45e18980a8b360d7bc7bad6c7ef
Nick Meinhold <nick@enspyr.co>
2025-11-26 14:30:05 +1100

    Complete 12.19

--- todo-app/todo-frontend/vite.config.js
+++ todo-app/todo-frontend/vite.config.js
@@ -4,8 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Listen on all addresses
-    allowedHosts: ["frontend", "localhost"],
-  },
 });
